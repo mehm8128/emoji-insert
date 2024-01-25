75d654b08954154d0830a4ca8c99bc8b3e0b8354
mehm8128 <mehm8128@gmail.com>
2024-01-26 00:47:52 +0900

    複数登録できるように

--- insertEmoji.js
+++ insertEmoji.js
@@ -1,7 +1,15 @@
+emojiMap = {
+	pray: "🙏",
+	ok_hand: "👌",
+}
+
 textareaList = document.getElementsByTagName("textarea")
 for (let i = 0; i < textareaList.length; i++) {
-  textareaList[i].oninput = (e) => {
-    const newText = e.target.value.replace(/:pray:$/, "🙏")
-    textareaList[i].value = newText
-  }
+	textareaList[i].oninput = (e) => {
+		const newText = Object.entries(emojiMap).reduce((prev, [key, value]) => {
+			const regexp = new RegExp(`:${key}:`)
+			return prev.replace(regexp, value)
+		}, e.target.value)
+		textareaList[i].value = newText
+	}
 }
